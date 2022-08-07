206c17bc16732f3b627ff70460320577beaf03f4
UserAgent02 <andrewleniuk@gmail.com>
2022-08-07 21:49:03 +0300

    8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form")
+form.addEventListener('submit', handleSubmit)
 
+function handleSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: {email, password },
+    } = event.currentTarget
+if (email.value === ''|| password.value ==='') {
+    return alert('You must fill all fields')
+}
+    const userInfo = { email: email.value, password: password.value }
+    console.log(userInfo);
+    event.currentTarget.reset()
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeButton = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const colorStyle = document.querySelector('span.color');
+
+changeButton.addEventListener('click', elem => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorStyle.textContent = color;
+
+})
